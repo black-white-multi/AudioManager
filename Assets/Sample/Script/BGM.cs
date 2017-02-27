050608ae500de23ae083c44f13f282d056842dd5
haratatsurou <hara.tatsurou@gmail.com>
2017-02-27 18:57:54 +0900

    first commit

---
+++ Assets/Sample/Script/BGM.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour {
+    public void StartBGM () {
+        AudioManager.Instance.PlayBGM ("sample");
+    }
+
+    public void StopBGM () {
+        AudioManager.Instance.StopBGM ();
+    }
+}
